77051edffa9457005dec4ebefb11a1ec7323de89
Henrik Grotle <henrik@pswin.com>
2012-10-04 10:03:30 +0200

    Added phase logic

--- TickTackApp/MainWindow.xaml.cs
+++ TickTackApp/MainWindow.xaml.cs
@@ -29,31 +29,44 @@ namespace TickTackApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private State isRunning = State.NotStarted;
+        private State timerState = State.NotStarted;
+        private Phase pomodoroPhase = Phase.Pause;
         private Storyboard storyboard;
 
         public MainWindow()
         {
             InitializeComponent();
-
             storyboard = (Storyboard)FindResource("arcStoryboard");
 
-            var duration = TimeSpan.FromMinutes(25);
+            SetDuration(TimeSpan.FromMinutes(3));
 
+            storyboard.Completed += storyboard_Completed;
+
+            Controls.Visibility = System.Windows.Visibility.Hidden;            
+        }
+
+        void storyboard_Completed(object sender, EventArgs e)
+        {
+            timerState = State.NotStarted;
+        }
+
+        private void SetDuration(TimeSpan duration)
+        {
+            storyboard.Duration = duration;
             foreach (var anim in storyboard.Children)
             {
                 anim.Duration = duration;
                 if (anim.Name == "boolAnim")
                 {
-                    ((BooleanAnimationUsingKeyFrames)anim).KeyFrames[1].KeyTime = TimeSpan.FromSeconds(duration.TotalSeconds / 2); 
+                    ((BooleanAnimationUsingKeyFrames)anim).KeyFrames[1].KeyTime = TimeSpan.FromSeconds(duration.TotalSeconds / 2);
                 }
             }
-            Controls.Visibility = System.Windows.Visibility.Hidden;            
         }
 
         private void Path_MouseDown_1(object sender, MouseButtonEventArgs e)
         {
-            DragMove();
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
         }
 
         private void Rectangle_MouseEnter_1(object sender, MouseEventArgs e)
@@ -75,31 +88,43 @@ namespace TickTackApp
         {
             storyboard.Duration = TimeSpan.FromMinutes(2);
 
-            switch (isRunning)
+            switch (timerState)
             {
                 case State.NotStarted:
-                    storyboard.Begin();
-                    pie.Visibility = System.Windows.Visibility.Visible;
-                    isRunning = State.Running;
+                    StartPhase();
                     break;
                 case State.Running:
                     storyboard.Pause();
-                    isRunning = State.Paused;
+                    timerState = State.Paused;
                     break;
                 case State.Paused:
                     storyboard.Resume();
-                    isRunning = State.Running;
+                    timerState = State.Running;
                     break;
                 default:
                     break;
             }
         }
 
+        private void StartPhase()
+        {
+            if (pomodoroPhase == Phase.Pause)
+                pomodoroPhase = Phase.Working;
+            else
+                pomodoroPhase = Phase.Pause;
+
+            var color = (Color)FindResource(pomodoroPhase.ToString());
+            pie.Fill = new SolidColorBrush(color);
+
+            storyboard.Begin();
+            pie.Visibility = System.Windows.Visibility.Visible;
+            timerState = State.Running;
+        }
         private void Reset_MouseUp(object sender, MouseButtonEventArgs e)
         {
             storyboard.Begin();
             pie.Visibility = System.Windows.Visibility.Visible;
-            isRunning = State.Running;
+            timerState = State.Running;
 
         }
 
@@ -110,7 +135,7 @@ namespace TickTackApp
                 anim.From = anim.To;
                 double newscale = (anim.From + ((e.Delta > 0) ? 0.10 : -0.10)).Value;
 
-                anim.To = Math.Min(10, Math.Max(0.25, newscale));
+                anim.To = Math.Min(1, Math.Max(0.25, newscale));
             }
             storyboard.Begin();
         }
