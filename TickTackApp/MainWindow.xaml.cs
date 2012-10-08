460c50693ecd02454ee91e3842f6e592c8faf781
Henrik Grotle <henrik@pswin.com>
2012-10-08 09:22:04 +0200

    added icon

--- TickTackApp/MainWindow.xaml.cs
+++ TickTackApp/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
-using Settings = TickTackApp.Properties.Settings;
+using Settings = TickTack.Properties.Settings;
 
 namespace TickTackApp
 {
--- TickTackApp/Properties/Settings.Designer.cs
+++ TickTackApp/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TickTackApp.Properties {
+namespace TickTack.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
